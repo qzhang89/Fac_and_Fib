5125a975ce55ddc676527f9ecce79a83d46933f1
Qian Zhang <q.zhang@grs-sim.de>
2019-11-20 16:12:55 +0100

    Add fac_and_fib.py

---
+++ fac_and_fib.py
@@ -0,0 +1,10 @@
+from math import factorial
+
+def fac(n):
+    return factorial(n)
+
+# Test
+print("fac(0)=%d" % fac(0))
+print("fac(1)=%d" % fac(1))
+print("fac(2)=%d" % fac(2))
+print("fac(3)=%d" % fac(3))
